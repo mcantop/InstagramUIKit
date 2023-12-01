243951db98dc74c131f636f01b78007a6161f5d1
mcantop <maciejcantop@icloud.com>
2023-12-01 09:56:28 +0100

    Project setup

---
+++ InstagramUIKit/Core/Auth/LoginController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginController.swift
+//  InstagramUIKit
+//
+//  Created by Maciej on 30/11/2023.
+//
+
+import UIKit
+
+final class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()        
+    }
+}
+
